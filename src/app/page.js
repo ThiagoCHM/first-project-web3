ecd5a1985e0030fe076d61f386d2cadf741b079e
Thiago <thiago@localhost.localdomain>
2024-06-09 16:06:07 -0300

    Projeto Base com Next

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,54 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { useEffect, useState } from "react";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { getOpenRequests } from "@/services/Web3Service";
+import Request from "@/components/Request";
 
 export default function Home() {
+
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    loadRequests(0);
+  }, [])
+
+  async function loadRequests(lastId) {
+    try {
+      const result = await getOpenRequests(lastId);
+      if (lastId === 0)
+        setRequests(result);
+      else {
+        requests.push(...result);
+        setRequests(requests);
+      }
+    }
+    catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Header />
+      <div className="container">
+        <div className="row ps-5">
+          <p className="lead m-4">Apoie o projeto de desenvolvimento de uma plataforma de crowdfunding para o Brasil. O projeto é uma plataforma de crowdfunding para o Brasil que visa ajudar pessoas a encontrarem oportunidades de emprego e empreendedorismo do Brasil.</p>
         </div>
+        <div className="p-4 mx-5">
+          <div className="list-group">
+            {
+              requests && requests.length
+                ? requests.map(rq => <Request key={rq.id} data={rq} />)
+                : <>Conect sua carteira MetaMask no botão "Entrar" para ajudar ou pedir ajuda.</>
+            }
+          </div>
+        </div>
+        <Footer />
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
